56fc0baa1604aa3b84cf7242b7367a35e76ce69c
lizhenliang <zhenliang369@163.com>
2021-11-28 17:34:25 +0800

    Update docker_build.sh

--- k8s/docker_build.sh
+++ k8s/docker_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 docker_registry=192.168.31.70
+# 存储登录Harbor认证信息
 kubectl create secret docker-registry registry-pull-secret --docker-server=$docker_registry --docker-username=admin --docker-password=Harbor12345 --docker-email=admin@ctnrs.com -n ms
 
 service_list="eureka-service gateway-service order-service product-service stock-service portal-service"
@@ -13,6 +14,7 @@ mvn clean package -Dmaven.test.skip=true
 
 for service in $service_list; do
    cd $work_dir/$service
+   # 业务程序需进入biz目录里构建
    if ls |grep biz &>/dev/null; then
       cd ${service}-biz
    fi
@@ -20,6 +22,7 @@ for service in $service_list; do
    image_name=$docker_registry/microservice/${service}:$(date +%F-%H-%M-%S)
    docker build -t ${image_name} .
    docker push ${image_name} 
+   # 修改yaml中镜像地址为新推送的，并apply
    sed -i -r "s#(image: )(.*)#\1$image_name#" ${current_dir}/${service}.yaml
    kubectl apply -f ${current_dir}/${service}.yaml
 done
