3ef5f7cbbf8495ee324f1b7542dea40c58ac21f1
lizhenliang <zhenliang369@163.com>
2021-12-15 20:41:34 +0800

    Update product.sql

--- db/product.sql
+++ db/product.sql
@@ -2,7 +2,8 @@
 SQLyog Ultimate v12.08 (64 bit)
 MySQL - 5.6.43 : Database - tb_product
 *********************************************************************
-*/
+*/
+
 
 /*!40101 SET NAMES utf8 */;
 
@@ -25,7 +26,7 @@ CREATE TABLE `product` (
 
 /*Data for the table `product` */
 
-insert  into `product`(`id`,`product_name`,`price`) values (1,'测试商品1',99.990),(2,'美女',999.000),(3,'Q币',100.000),(4,'貂皮大衣很厚很厚的那种',9999.000);
+insert  into `product`(`id`,`product_name`,`price`) values (1,'手机',3999.990),(2,'电脑',9999.000),(3,'充气娃娃',600.000),(4,'枸杞',49.000);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
