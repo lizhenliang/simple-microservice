56e8ed41e3f16b4a43fffb138a99c99a2dfefac6
lizhenliang <zhenliang369@163.com>
2023-07-02 11:17:12 +0800

    Create docker_build.sh

---
+++ docker_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+docker_registry=192.168.1.90
+service_list="eureka-service gateway-service order-service product-service stock-service portal-service"
+service_list=${1:-${service_list}}
+work_dir=$(dirname $PWD)
+current_dir=$PWD
+
+cd $work_dir
+mvn clean package -Dmaven.test.skip=true
+
+for service in $service_list; do
+   cd $work_dir/$service
+   # 业务程序需进入biz目录里构建
+   if ls |grep biz &>/dev/null; then
+      cd ${service}-biz
+   fi
+   service=${service%-*}
+   image_name=$docker_registry/microservice/${service}:$(date +%F-%H-%M-%S)
+   docker build -t ${image_name} .
+   docker push ${image_name} 
+done
